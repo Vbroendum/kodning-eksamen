b93a4256eeb6ec6217911a1649880aff513e684c
Vbroendum <32537066+Vbroendum@users.noreply.github.com>
2024-05-24 12:39:20 +0200

    update

--- script.js
+++ script.js
@@ -120,13 +120,4 @@ function handleNextButton(){
 
 
 //Hvis der stadig er spørgsmål tilbage vil den vise de næste spørgsmål, hvis der ikke er flere tilbage vil den genstarte quizzen
-nextButton.addEventListener("click", ()=> {
-    if(currentQuestionIndex < questions.length){
-        handleNextButton(); 
-    }else{
-        startQuiz();
-    }
-});
-
-//kalder og eksekverer funktionen startQuiz
-startQuiz();
+nextButton.addEventListener("c
